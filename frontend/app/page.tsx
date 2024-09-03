8e2c34d50d9519d807313c673f46f6c54b915073
cu8code <git8ankanroy@gmail.com>
2024-09-03 13:46:38 +0530

    added login

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,6 +9,7 @@ import ProjectList from "@/components/ProjectList";
 import FileUploadSection from "@/components/FileUploadSection";
 import ApiKeySection from "@/components/ApiKeySection";
 import config from "@/lib/config";
+import ProtectedPage from "@/components/ProtectedPage";
 
 export default function Home() {
   const { data: projects, isLoading, isError, refetch } = useQuery({
@@ -21,52 +22,54 @@ export default function Home() {
   });
 
   return (
-    <main className="flex flex-col min-h-screen bg-gray-100">
-      <motion.div
-        className="flex-1 grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 lg:p-8"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        <Card className="w-full h-full">
-          <CardHeader>
-            <CardTitle className="text-xl lg:text-2xl font-bold flex items-center">
-              <FiList className="mr-2" /> Projects
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <ProjectList projects={projects?.data} isLoading={isLoading} isError={isError} />
-          </CardContent>
-        </Card>
-
-        <div className="flex flex-col gap-4">
+    <ProtectedPage>
+      <main className="flex flex-col min-h-screen bg-gray-100">
+        <motion.div
+          className="flex-1 grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 lg:p-8"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
           <Card className="w-full h-full">
             <CardHeader>
               <CardTitle className="text-xl lg:text-2xl font-bold flex items-center">
-                <FiUpload className="mr-2" /> Upload CSV
+                <FiList className="mr-2" /> Projects
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <FileUploadSection
-                refetch={refetch}
-              />
+              <ProjectList projects={projects?.data} isLoading={isLoading} isError={isError} />
             </CardContent>
           </Card>
 
-          <div className="flex flex-col lg:flex-row w-full h-full gap-5">
-            <Card className="w-full">
+          <div className="flex flex-col gap-4">
+            <Card className="w-full h-full">
               <CardHeader>
                 <CardTitle className="text-xl lg:text-2xl font-bold flex items-center">
-                  <FiSettings className="mr-2" /> API Keys
+                  <FiUpload className="mr-2" /> Upload CSV
                 </CardTitle>
               </CardHeader>
               <CardContent>
-                <ApiKeySection />
+                <FileUploadSection
+                  refetch={refetch}
+                />
               </CardContent>
             </Card>
+
+            <div className="flex flex-col lg:flex-row w-full h-full gap-5">
+              <Card className="w-full">
+                <CardHeader>
+                  <CardTitle className="text-xl lg:text-2xl font-bold flex items-center">
+                    <FiSettings className="mr-2" /> API Keys
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ApiKeySection />
+                </CardContent>
+              </Card>
+            </div>
           </div>
-        </div>
-      </motion.div>
-    </main>
+        </motion.div>
+      </main>
+    </ProtectedPage>
   );
 }
